e1f26db087f552c74ca0c4582617ca90d8e1065b
Oscar Serrano <137729132+OSCARSO17@users.noreply.github.com>
2025-04-22 09:55:08 -0600

    Create Estudiante

---
+++ MovimientoEstudiantil/Models/Estudiante.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Estudiante
+{
+    public int IdEstudiante { get; set; }
+
+    public string Correo { get; set; }
+
+    public string Provincia { get; set; }
+
+    public string Sede { get; set; }
+
+    public string SatisfaccionCarrera { get; set; } 
+
+    public int AnioIngreso { get; set; }
+}
