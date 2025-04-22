8d4aa733f21e7f2a2b6b3484e8e8c0f2f5f8484d
Oscar Serrano <137729132+OSCARSO17@users.noreply.github.com>
2025-04-22 10:06:26 -0600

    Create Models

---
+++ MovimientoEstudiantil/Models/HistorialRegistro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class HistorialRegistro
+{
+    public int IdHistorial { get; set; }
+
+    public int IdUsuario { get; set; }
+
+    public string Accion { get; set; }
+
+    public string Descripcion { get; set; } 
+
+    public DateTime FechaRegistro { get; set; }
+
+    public TimeSpan Hora { get; set; }
+
+    public string Rol { get; set; } 
+}
---
+++ MovimientoEstudiantil/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Usuario
+{
+    public int IdUsuario { get; set; }
+
+    public string Correo { get; set; }
+
+    public string Sede { get; set; }
+
+    public string Contrasena { get; set; }
+
+    public string Rol { get; set; } 
+
+    public DateTime FechaRegistro { get; set; }
+}
